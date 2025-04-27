e48d0eaa991d3e4b65dfee988063d93ab91994b8
K Sai Sumanth Reddy <reddysaisumanth8@gmail.com>
2025-04-27 15:05:34 -0400

    new changes

--- server/index.js
+++ server/index.js
@@ -1,35 +1,4 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
 
-// dotenv.config();
-// const app = express();
-
-// require('dotenv').config();  
-
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-
-
-// app.use(cors());
-// app.use(express.json());
-
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() =>{ console.log('MongoDB connected');})
-//   .catch(err => console.error('MongoDB connection error:', err));
-
-
-// app.use('/api/devices', require('./routes/devices'));
-// app.use('/api/upload', require('./routes/upload'));
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -41,7 +10,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: 'https://sbom-viewer.onrender.com/',  // ✅ Allow your frontend
+  origin: 'https://sbom-viewer.onrender.com',  // ✅ Allow your frontend
   credentials: true,
 };
 
